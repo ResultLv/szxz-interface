37075c848e5623a63ab06041faa3e948cd22888e
ResultLv <553666725@qq.com>
2019-10-13 08:51:18 +0800

    update

--- src/main/java/com/example/demo/controller/MouldQueryController.java
+++ src/main/java/com/example/demo/controller/MouldQueryController.java
@@ -1,18 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.JsonResult;
-import com.example.demo.entity.Mould;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.MouldMapper;
 import com.example.demo.service.MouldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class MouldQueryController {
